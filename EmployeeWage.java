6aa942e985e1ec0076d13a69f9ac4f6bc5214b3d
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-04-13 19:04:15 +0530

    ADD:add part time employee wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,21 +4,24 @@ class EmployeeWage
 {
 public static void main(String args[])
 {
-Random r = new Random();
-int empCheck = r.nextInt(2);
+Random r=new Random();
+int empcheck=r.nextInt(2);
+int counter;
+int Present_full_time=1;
+int Present_part_time=2;
 int emp_rate_per_hr=20;
-int emphrs = 8;
-int salary = 0;
+int total_working_days=20;
+int emphrs=8;
+int salary=0;
 
-if(empCheck == emp_rate_per_hr)
+for(counter=1; counter<=total_working_days; counter++)
 {
-        System.out.println("emphrs * emp_rate_per_hr");
+        salary=(emphrs * emp_rate_per_hr);
 }
-else
-{
-        System.out.println("salary = 0");
+System.out.println("Salary = "+ salary);
 }
 }
-}
+
+
 
 
