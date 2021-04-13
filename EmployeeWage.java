795dd03481be1aba0521e3c4de3b274f7b17a2dc
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-04-13 18:38:59 +0530

    ADD:emp present or absent

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,7 +1,20 @@
+import java.util.Random;
+
 class EmployeeWage
 {
-public static void main(String args[])
+public static void main(String args [])
 {
-System.out.println("Welcome to employee wage problem");
+Random r = new Random();
+int empCheck = r.nextInt(2);
+int is_full_time=1;
+if (empCheck == is_full_time)
+{
+        System.out.println(" Employee is present");
+}
+else
+{
+        System.out.println(" Employee is absent");
 }
 }
+}
+
